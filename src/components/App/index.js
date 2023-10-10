60982d230cc9bc6f30adcdf46da0bcac31225574
Srijan Khairgoli Shrestha <srijanshrestha0321@gmail.com>
2023-10-10 15:17:45 +0545

    Initial Commit

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from '../Login/index.js';
+import Dashboard from '../Dashboard/index.js';
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    setIsAuthenticated(JSON.parse(localStorage.getItem('is_authenticated')));
+  }, []);
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Dashboard setIsAuthenticated={setIsAuthenticated} />
+      ) : (
+        <Login setIsAuthenticated={setIsAuthenticated} />
+      )}
+    </>
+  );
+};
+
+export default App;
